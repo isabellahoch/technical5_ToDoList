8971d58a77623b52cbaa992a7b76100ccee8c533
isabellahoch <ihochschild@gmail.com>
2019-07-12 14:40:24 -0700

    complete iteration 0

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Apple on 7/12/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
